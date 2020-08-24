cfbf21cda8ad762403f495d0d610a33340ef270c
Manoli <igielskv@icloud.com>
2020-08-24 07:24:30 +0200

    Setting up initial ContentView layout.

--- RPS/ContentView.swift
+++ RPS/ContentView.swift
@@ -10,7 +10,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 50.0) {
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+                .font(.system(size: 70.0))
+            
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+                .font(.title)
+            
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text(Sign.rock.emoji)
+                        .font(.system(size: 70.0))
+                }
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text(Sign.paper.emoji)
+                        .font(.system(size: 70.0))
+                }
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text(Sign.scissors.emoji)
+                        .font(.system(size: 70.0))
+                }
+            }
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Play Again")
+                    .font(.title)
+            }
+        }
     }
 }
 
