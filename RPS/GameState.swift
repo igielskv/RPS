940546d722341d97694bddaf38c979927887ac41
Manoli <igielskv@icloud.com>
2020-08-24 07:09:42 +0200

    Creates Model enums with required logic to play RPS game.

---
+++ RPS/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Manoli on 23/08/2020.
+//  Copyright © 2020 macForce.one. All rights reserved.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw
+}
