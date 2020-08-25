cc27589a88325ededfd8446a356d82d4d9ae58e9
Manoli <igielskv@icloud.com>
2020-08-25 06:57:56 +0200

    Moves backgroundColors dictionary to ContentView as it's related to the view.

--- RPS/ContentView.swift
+++ RPS/ContentView.swift
@@ -15,6 +15,13 @@ struct ContentView: View {
     @State var computerSign: Sign?
     @State var playerSign: Sign?
     
+    let backgroundColors: [GameState : Color] = [
+        .start : Color(.systemGray),
+        .win : Color(red: 0.651, green: 0.792, blue: 0.651),
+        .lose : Color(red: 0.851, green: 0.424, blue: 0.412),
+        .draw : Color(red: 0.663, green: 0.663, blue: 0.663)
+    ]
+    
     var body: some View {
         ZStack {
             backgroundColors[gameState]
